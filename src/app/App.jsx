bc7a39968133dc7fe6755308887c272f576063bc
ariful4082 <ariful4082@gmail.com>
2021-04-01 00:57:33 +0600

    convert html to react stack bucket page

---
+++ src/app/App.jsx
@@ -0,0 +1,25 @@
+import { Helmet } from "react-helmet";
+
+
+//custom Components
+import BucketPage from "../pages/buckets";
+
+function App() {
+  return (
+    <div className="App">
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,500&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;1,300&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <BucketPage/>
+    </div>
+  );
+}
+
+export default App;
